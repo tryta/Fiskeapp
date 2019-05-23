117def5c54616729faeea1f8a0a0c5d0ba00e21b
tryta <47241882+tryta@users.noreply.github.com>
2019-05-23 11:23:44 +0200

    Hei

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+function showMainScreen() {
+    }
+    mainextra.innerHTML = `
+      <div><button class="button-content" onclick="()">Fyll inn rapport</button></div>
+      <div><button class="button-content" onclick="()">Oversikt over fangst</button></div>
+      <div><button class="button-content" onclick="()">Oversikt over fiskeplasser</button></div>
+      <div><button class="button-content" onclick="()">Blogg</button></div>
+      <div><button class="button-content" onclick="()">Annet</button></div>
+    `;
